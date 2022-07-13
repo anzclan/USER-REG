9d1667e3dd531da09b2af2b73496aa17a9211a89
aNz <107313945+anzclan@users.noreply.github.com>
2022-07-13 22:55:08 +0800

    Create anz.sh

---
+++ anz.sh
@@ -0,0 +1,70 @@
+#MENU
+cp /usr/bin/menu /root/backup/menu/menu.sh
+cp /usr/bin/menu-xtls /root/backup/menu/-xtls.sh
+cp /usr/bin/menu-wg /root/backup/menu/menu-wg.sh
+cp /usr/bin/menu-vmess /root/backup/menu/menu-vmess.sh
+cp /usr/bin/menu-vless /root/backup/menu/menu-vless.sh
+cp /usr/bin/menu-trojan /root/backup/menu/menu-trojan.sh
+cp /usr/bin/menu-sys /root/backup/menu/menu-sys.sh
+cp /usr/bin/menu-ssrmu /root/backup/menu/menu-ssrmu.sh
+cp /usr/bin/menu-ssr /root/backup/menu/menu-ssr.sh
+cp /usr/bin/menu-ssh /root/backup/menu/menu-ssh.sh
+
+#MEMBER
+cp /usr/bin/member /root/backup/member.sh
+cp /usr/bin/usernew /root/backup/usernew.sh
+cp /usr/bin/user-xvless /root/backup/user-xvless.sh
+cp /usr/bin/user-xws /root/backup/user-xws.sh
+cp /usr/bin/user-xtls /root/backup/user-xtls.sh
+cp /usr/bin/user-trxtls /root/backup/user-trxtls.sh
+cp /usr/bin/user-trgrpc /root/backup/user-trgrpc.sh
+cp /usr/bin/user-trgfw /root/backup/user-trgfw.sh
+cp /usr/bin/user-limit /root/backup/user-limit.sh
+
+#ADD
+cp /usr/bin/add-xws /root/backup/add-xws.sh
+cp /usr/bin/add-xvless /root/backup/add-xvless.sh
+cp /usr/bin/add-xtls /root/backup/add-xtls.sh
+cp /usr/bin/add-wg /root/backup/add-wg.sh
+cp /usr/bin/add-trxtls /root/backup/add-trxtls.sh
+cp /usr/bin/add-tr /root/backup/add-tr.sh
+cp /usr/bin/add-ssr /root/backup/add-ssr.sh
+cp /usr/bin/add-trgrpc /root/backup/add-trgrpc.sh
+cp /usr/bin/add-ss /root/backup/add-ss.sh
+cp /usr/bin/add-host /root/backup/add-host.sh
+
+#UPDATE
+cp /usr/bin/update /root/backup/update.sh
+
+#TRIAL
+cp /usr/bin/trial-xws /root/backup/trialxws-.sh
+cp /usr/bin/trial-xtls /root/backup/trial-xtls.sh
+cp /usr/bin/trial-wg /root/backup/trial-wg.sh
+cp /usr/bin/trial-trxtls /root/backup/trial-trxtls.sh
+cp /usr/bin/trial-trgrpc /root/backup/trial-trgrpc.sh
+cp /usr/bin/trial-tr /root/backup/trial-tr.sh
+cp /usr/bin/trial /root/backup/trial.sh
+
+#RENEW
+cp /usr/bin/renew-xws /root/backup/renew-xws.sh
+cp /usr/bin/renew-xvless /root/backup/renew-xvless.sh
+cp /usr/bin/renew-xtls /root/backup/renew-xtls.sh
+cp /usr/bin/renew-wg /root/backup/renew-wg.sh
+cp /usr/bin/renew-trxtls /root/backup/renew-trxtls.sh
+cp /usr/bin/renew-trgrpc /root/backup/renew-trgrpc.sh
+cp /usr/bin/renew-tr /root/backup/renew-tr.sh
+cp /usr/bin/renew-ssr /root/backup/renew-ssr.sh
+cp /usr/bin/renew-ss /root/backup/renew-ss.sh
+cp /usr/bin/renew /root/backup/renew.sh
+
+
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
+cp /usr/bin/menu /root/backup/menu.sh
